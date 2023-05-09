731edba914fedc1df4f8ada220d3caec99e7d4e5
Andrew <71883814+AndrewB1998@users.noreply.github.com>
2023-05-10 02:25:56 +0300

    Updated the ui, join functions and fixes

--- SecureChat.py
+++ SecureChat.py
@@ -9,86 +9,106 @@ hostname = socket.gethostname()
 HOST = socket.gethostbyname(hostname)
 PORT = 1234
 
-
-
 # Create the Tk instance
 root = Tk()
 root.title("Chatter")
 root.resizable(False, False)
-root.geometry("600x300")
+root.geometry("600x400")
 class StartUI:
     def __init__(self, root):
         self.root = root
         self.frames()
-        self.create_left_frame()
-        self.create_right_frame()
-
+        self.HOST = ""
+        self.PORT = ""
+        self.name = ""
+        
     def frames(self):
         self.root.columnconfigure(0, weight=1)
-        self.root.columnconfigure(1, weight=1)
-        self.root.rowconfigure(0, weight=1)
         
-        self.left_frame = Frame(self.root, bg="lightblue")
-        self.left_frame.grid(column=0, row=0, sticky="NSEW")
-        self.left_frame.columnconfigure(0, weight=1)
+        self.root.columnconfigure(4, weight=1)    
+        for i in range(5):
+            self.root.rowconfigure(i, weight=1)
         
-        self.right_frame = Frame(self.root)
-        self.right_frame.grid(column=1, row=0)
+        self.left_frame = Frame(self.root, bg="lightblue").grid(column=0, rows=5, sticky="NSEW")
+        self.right_frame = Frame(self.root).grid(column=1, row=0)
+        
+        def set_name():
+            self.name = self.name_entry.get()
+            self.join_btn.configure(text=f"Join as {self.name}, IP: {self.HOST}, Port: {self.PORT})")
+            self.host_btn.configure(text=f"Host as {self.name}, IP: {self.HOST}, Port: {self.PORT})")
+            if len(self.name.strip()) > 0 and self.HOST != "" and self.PORT != "":
+                self.join_btn.configure(state=NORMAL)
+                self.host_btn.configure(state=NORMAL)
+            
+        def set_ip():
+            self.HOST= self.ip_entry.get()
+            self.join_btn.configure(text=f"Join as {self.name}, IP: {self.HOST})")
+            self.host_btn.configure(text=f"Host as {self.name}, IP: {self.HOST})")
+            if len(self.name.strip()) > 0 and self.HOST != "" and self.PORT != "":
+                self.join_btn.configure(state=NORMAL)
+                self.host_btn.configure(state=NORMAL)
+            print(self.HOST)
+            
+            
+            
+        
+        def set_port():
+            self.PORT = int(self.port_entry.get())
+            self.join_btn.configure(text=f"Join as {self.name}, IP: {self.HOST}, Port: {self.PORT})")
+            self.host_btn.configure(text=f"Host as {self.name}, IP: {self.HOST}, Port: {self.PORT})")
+            if len(self.name.strip()) > 0 and self.HOST != "" and self.PORT != "":
+                self.join_btn.configure(state=NORMAL)
+                self.host_btn.configure(state=NORMAL)
+            
+        def make_element(element,frame, text, bg, font, row, column, padx, sticky, columnspan):
+            self.name = element(frame, text=text, bg=bg, font=font)
+            self.name.grid(row=row, column=column, padx=padx, sticky=sticky, columnspan=columnspan)
+            return self.name
+        
+        def make_button(frame, text, command, state):
+            self.name = Button(frame, text=text, command=command, state=state)
+            return self.name
+         
+               
+        self.title_lbl = make_element(Label, self.left_frame, "ChatBox", "lightblue", "Constantia 20 bold", 0, 0, 40, "",1)
+        self.desc_lbl = make_element(Label, self.left_frame, "Host or join a chat \n Leave IP and Port empty if hosting ", "lightblue", "Constantia 10", 2, 0, 0, "",1)
+        self.name_lbl = make_element(Label, self.right_frame, "Username", "SystemButtonFace","TkDefaultFont", 0, 1, 0, "",1)
+        self.ip_lbl = make_element(Label, self.right_frame, f"IP (Local:{HOST})", "SystemButtonFace", "TkDefaultFont", 1, 1, 0, "",1)
+        self.port_lbl = make_element(Label, self.right_frame, "Port (Default: 1234)", "SystemButtonFace", "TkDefaultFont", 2, 1, 0, "",1)
+        
+        self.name_entry = make_element(Entry, self.right_frame, "", "White", "TkDefaultFont", 0, 2, 5, "",1)
+        self.ip_entry = make_element(Entry, self.right_frame, "", "White", "TkDefaultFont", 1, 2, 5, "",1)
+        self.port_entry = make_element(Entry, self.right_frame, "", "White", "TkDefaultFont", 2, 2, 5, "",1)
+        
+        self.name_btn = make_button(self.right_frame, "Save", set_name, NORMAL)  
+        self.ip_btn = make_button(self.right_frame, "Save", set_ip, NORMAL)
+        self.port_btn = make_button(self.right_frame, "Save", set_port, NORMAL)
+        self.join_btn = make_button(self.right_frame, "Join",lambda: raise_frame_join(), DISABLED)
+        self.host_btn = make_button(self.right_frame, "Host",lambda: raise_frame_host(self.root), DISABLED)
+        
+        self.name_btn.grid(row=0, column=4, padx=5, sticky="EW", columnspan=2)
+        self.ip_btn.grid(row=1, column=4, padx=5, sticky="EW", columnspan=2)
+        self.port_btn.grid(row=2, column=4, padx=5, sticky="EW", columnspan=2)
+        self.join_btn.grid(row=3, column=1, padx=0, sticky="NSEW", columnspan=4)
+        self.host_btn.grid(row=4, column=1, padx=0, sticky="NSEW", columnspan=4)
 
-    def create_left_frame(self):
-        self.logo_label = Label(self.left_frame, text= "ChatBox", bg="lightblue", font="Constantia 20 bold")
-        self.logo_label.grid(row=0, column=0, pady=20, padx=40, sticky="NSEW")
-        self.description = Label(self.left_frame, text="Host or join a chat \n Leave IP and Port empty if hosting ", bg="lightblue", font="Constantia 10")
-        self.description.grid(row=1, column=0, sticky="NSEW")
-    
-    def create_right_frame(self):
+           
         def raise_frame_join():
-            btn_forget = [self.left_frame, self.right_frame, self.join_btn, self.host_btn, self.name_label, self.name_entry, self.save_name, self.ip_addr, self.ip_addr_entry, self.port_num, self.port_num_entry, self.port_save, self.ip_save, self.empty_row, self.logo_label]
-            for btn in btn_forget:
-                btn.grid_forget()
-            self.client = Client(HOST, PORT, root, self.name)
+            for widget in root.winfo_children():
+                widget.destroy()
+            self.client = Client(self.HOST, self.PORT, root, self.name)
 
         def raise_frame_host(frame):
             pass   
 
-        def set_name():
-            self.name = self.name_entry.get()
-            self.join_btn.configure(text=f"Join chat as {self.name}")
-            self.host_btn.configure(text=f"Host a chat as {self.name}")
-            self.join_btn.configure(state=NORMAL), self.host_btn.configure(state=NORMAL)
-
-        self.name_label = Label(self.right_frame, text="Username")
-        self.name_entry = Entry(self.right_frame)
-        self.save_name = Button(self.right_frame, text="Save", command=set_name, padx=5)
-
-        self.ip_addr = Label(self.right_frame, text=f"IP: (Local:{HOST})")
-        self.ip_addr_entry = Entry(self.right_frame)
-        self.ip_save = Button(self.right_frame, text="Save", padx=5)
-
-        self.port_num = Label(self.right_frame, text="Port: (Default: 1234)")
-        self.port_num_entry = Entry(self.right_frame)
-        self.port_save = Button(self.right_frame, text="Save", padx=5)
-
-        self.join_btn = Button(self.right_frame, text="Join", command=lambda: raise_frame_join(), state=DISABLED, height=2)
-        self.host_btn = Button(self.right_frame, text="Host", command=lambda: raise_frame_host(self.root), state=DISABLED, height=2)
-        
-        self.empty_row = Label(self.right_frame, text="")
-        self.name_label.grid(row=0, column=0, sticky="W")
-        self.name_entry.grid(row=0, column=1, padx=5)
-        self.save_name.grid(row=0, column=2)
-        self.ip_addr.grid(row=1, column=0, sticky="W")
-        self.ip_addr_entry.grid(row=1, column=1)
-        self.ip_save.grid(row=1, column=2)
-        self.port_num.grid(row=2, column=0, sticky="W")
-        self.port_num_entry.grid(row=2, column=1)
-        self.port_save.grid(row=2, column=2)
-        self.empty_row.grid(row=3)
-        self.join_btn.grid(row=4, column=0, sticky="NSEW")
-        self.host_btn.grid(row=4, column=1, sticky="NSEW")
         
 class Client:
     def __init__(self, host, port, root, name):
+        self.host = host
+        self.port = port
         self.root = root
+        self.name = name
+        
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((host, port))
         
