0929925e92c9a89c470edad01aab742f9b407c1c
Andrew <71883814+AndrewB1998@users.noreply.github.com>
2023-05-13 00:40:09 +0300

    fernet key protection, encrypt mode + many changes

--- Server.py
+++ Server.py
@@ -1,10 +1,13 @@
 import socket
 import threading    
 import subprocess
+from cryptography.fernet import Fernet
+
 hostname = socket.gethostname()
 HOST = socket.gethostbyname(hostname)
 PORT = 1234
 
+key = Fernet.generate_key()
 
 # Create, bind and accept socket object (AF_INET = IPv4, SOCK_STREAM = TCP)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,37 +25,35 @@ def handle(client):
     while True:
         try:
             message = client.recv(1024)
-            print(message.decode('utf-8'))
             send(message)
             
             if len(names) > 0:
                 send_names = " ".join(names)
                 client.send(f"NAMES {send_names}".encode('utf-8'))
             
-        except:
+        except ConnectionResetError:
             idx = clients.index(client)
             clients.remove(client)
-            client.close()
             name = names[idx]
             names.remove(name)
-            send(f"{name} has left the chat!".encode('utf-8'))
-           
+            print(f"Client {name} left")
+            send(f"User left: {name}".encode('utf-8'))
             break
+    
+    
 
 def receive():
     while True:
         client, address = server.accept()
         print(f"Connected with {str(address)}")
-        
+        client.send(f"KEY {key}".encode('utf-8'))
+        print(key)
         client.send("NAME".encode('utf-8'))
         name = client.recv(1024).decode('utf-8')
-        
+        send(f"Welcome to the chat {name}!".encode('utf-8'))
         names.append(name)
         clients.append(client)
-        print(f"Name of the client is {name}")
-        send(f"{name} has joined the chat!".encode('utf-8'))
-        client.send("Connected to the server!".encode('utf-8'))
-        
+
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
         
